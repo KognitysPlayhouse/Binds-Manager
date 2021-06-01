bd22f9c98bd6df0ef2ee18f0374ca914ace54ecc
Kognity <kognitysplayhouse@gmail.com>
2021-05-31 22:47:17 -0500

    Revert "bap"
    This reverts commit 51393755b23b438170dd791dd31f3ef7f374c903.

--- BindsManager/BindsManager/Properties/AssemblyInfo.cs
+++ BindsManager/BindsManager/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6E7AE2B1-CB96-4B1C-9317-B72B6B4E3D97")]
